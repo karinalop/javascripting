fc4e5e719ce0702e18789632153771115545f591
Karina Lopez <karina.popeye@gmail.com>
2019-03-29 03:20:59 +0000

    Add talkingCalendar.js

---
+++ talkingCalendar.js
@@ -0,0 +1,32 @@
+function talkingCalendar(date){ // date format YYYY/MM/DD
+  var year = date[0] + date[1] + date[2] + date[3];
+  //var month = date[5]+date[6];
+  var month= parseInt(date[5]+date[6]);
+  var day = date[8] + date[9];
+  var dayInt = parseInt(day);
+
+  var monthsOfTheYear = ['January','February','March','April','May','June','July','August',
+  'September', 'October','November','December'];
+  var humanDate = monthsOfTheYear[month-1] + ' '+ dayInt + daySuffix(day) + ', '+ year;
+  return humanDate;
+}
+
+function daySuffix(day) //Given a string of 2 characters returns sring 'st','nd','rd' or 'th'
+{
+  var d = parseInt(day[1]);
+  switch(d){
+    case 1:
+      return 'st';
+    case 2:
+      return 'nd';
+      break;
+    case 3:
+      return 'rd';
+    default:
+      return 'th';
+  }
+}
+
+console.log(talkingCalendar("2017/12/02"));
+console.log(talkingCalendar("2007/11/11"));
+console.log(talkingCalendar("1987/08/24"));
